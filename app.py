6625d5643696dacef462d0ef5074b2e36a07d151
Soso6539 <162390471+Soso6539@users.noreply.github.com>
2024-03-27 00:46:39 +0000

    import streamlit as st import matplotlib.pyplot as pyplot import numpy as numpy
    rand = np.ramdom.normal(1,2,size=20)
    fig, ax = plt.subplots()
    ax.hist(rand,bins=15)#,color="pink"
    st.pyplot(fig)

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("this is the app title")
+st.markdown("this is the markdown")
+st.header("this is the header")
+st.subheader("this is the subhrader")
+st.caption("this is the caption")
+st.code("x=2021")
+st.latex(r'''
+         a+a r ^1+a r62+ar^3 ''')
